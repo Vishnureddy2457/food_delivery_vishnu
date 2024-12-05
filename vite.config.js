63b40860ff2f3c1ab53efb8a1d35c62dfce34ed5
Vishnureddy2457 <vishnuvishnu21801@gmail.com>
2024-12-05 12:57:57 +0530

    FOOD DELIVER APP

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'ignore-during-builds',
-          buildStart() {
-            if (process.env.VITE_IGNORE_DURING_BUILDS === 'true') {
-              console.log('Ignoring certain files during the build...');
-              // Perform additional logic or skip certain builds
-            }
-          }
-        }
-      ]
-    }
   
 })
